73eebdc393da30c007c5a2966eff3e65e998e942
Matthew <matthewgallan@gmail.com>
2025-09-03 02:20:51 +0100

    Sketched tile autotiling

---
+++ shapes.c
@@ -0,0 +1,216 @@
+#include <stdint.h>
+#include <stdio.h>
+
+const uint16_t big_tiles[16] = {
+    0x0000, 0x9000, 0x0700, 0x8800,
+    0x0030, 0x6060, 0x0730, 0xd860,
+    0x0001, 0x9001, 0x0404, 0x8a04,
+    0x0022, 0x60b2, 0x042c, 0x5555
+};
+
+const char tile_disp[14][8] = {
+    "        ", "+---| ##", "----####", "---+## |",
+    "| ##| ##", "########", "## |## |", "| ##+---",
+    "####----", "## |---+", "####+ ##", "## +####",
+    "+ ######", "###### +"
+};
+
+#define height 4
+#define width 4
+
+#define img_height (height * 2)
+#define img_width (width * 2)
+
+#define display_height (img_height * 2)
+#define display_width (img_width * 4)
+
+#define shape_count 20
+
+const uint8_t shapes[shape_count][height][width] = {
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0},
+        {1, 1, 1, 1},
+    },
+    {
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+        {1, 0, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 1},
+        {0, 0, 0, 1},
+        {0, 0, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 1},
+        {0, 0, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0},
+        {0, 1, 1, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 0, 0, 1},
+        {0, 1, 1, 1},
+        {0, 0, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 1},
+        {0, 1, 0, 0},
+        {0, 0, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+        {0, 0, 1, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 0, 0},
+        {0, 1, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 0, 1, 0},
+        {0, 1, 1, 1},
+        {0, 0, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 1},
+        {0, 0, 1, 0},
+        {0, 0, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 0, 1, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 1, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 0, 1, 1},
+        {0, 1, 1, 0},
+        {0, 0, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 1, 1, 0},
+        {0, 0, 1, 1},
+        {0, 0, 0, 0},
+    },
+    {
+        {0, 0, 0, 0},
+        {0, 0, 1, 0},
+        {0, 1, 1, 0},
+        {0, 1, 0, 0},
+    },
+    {
+        {1, 1, 1, 1},
+        {1, 0, 1, 1},
+        {0, 1, 0, 1},
+        {0, 1, 1, 0},
+    }
+};
+
+uint8_t image[img_height][img_width] = {0};
+
+uint8_t get_shape(int shape, int shp_x, int shp_y) {
+    if(shp_x < 0 || shp_x >= width || shp_y < 0 || shp_y >= height) {
+        return 0;
+    }
+    return shapes[shape][shp_y][shp_x];
+}
+
+void set_image(int img_x, int img_y, uint8_t val) {
+    if(img_x < 0 || img_x >= img_width || img_y < 0 || img_y >= img_height) {
+        return;
+    }
+    image[img_y][img_x] = val;
+}
+
+char get_disp_char(int dsp_x, int dsp_y) {
+    return tile_disp[image[dsp_y / 2][dsp_x / 4]][(dsp_x % 4) + ((dsp_y % 2) << 2)];
+}
+
+void print_shape(int shape) {
+    for(int y = 0; y < height + 1; y++) {
+        for(int x = 0; x < width + 1; x++) {
+            uint8_t big_tile = (
+                get_shape(shape, x - 1, y - 1) |
+                (get_shape(shape, x, y - 1) << 1) |
+                (get_shape(shape, x - 1, y) << 2) |
+                (get_shape(shape, x, y) << 3)
+            );
+            uint16_t mask = big_tiles[big_tile];
+            int img_x = x * 2, img_y = y * 2;
+            set_image(img_x - 1, img_y - 1, (mask & 0xF000) >> 12);
+            set_image(img_x    , img_y - 1, (mask & 0x0F00) >> 8 );
+            set_image(img_x - 1, img_y    , (mask & 0x00F0) >> 4 );
+            set_image(img_x    , img_y    , (mask & 0x000F) >> 0 );
+        }
+    }
+    printf("+");
+    for(int x = 0; x < display_width; x++) {
+        printf("-");
+    }
+    printf("+\n");
+    for(int y = 0; y < display_height; y++) {
+        printf("|");
+        for(int x = 0; x < display_width; x++) {
+            printf("%c", tile_disp[image[y / 2][x / 4]][(x % 4) + ((y % 2) << 2)]);
+        }
+        printf("|\n");
+    }
+    printf("+");
+    for(int x = 0; x < display_width; x++) {
+        printf("-");
+    }
+    printf("+\n");
+    printf("\n");
+}
+
+int main(int argc, char const *argv[]) {
+    for(int shape = 0; shape < shape_count; shape++) {
+        print_shape(shape);
+    }
+    return 0;
+}
+
+
